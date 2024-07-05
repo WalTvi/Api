886b5ae7bf1165f59dbabc84f44e21435813377c
WalTvi <107645695+WalTvi@users.noreply.github.com>
2024-07-05 10:50:11 -0400

    api.py

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z$pcke$0st*72#@p9moyxm_l%5-+(i-wue-r5hs0_e*#jy=hmk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.198.105.67', 'localhost']
+ALLOWED_HOSTS = ['34.228.82.253', 'localhost']
 
 
 # Application definition
